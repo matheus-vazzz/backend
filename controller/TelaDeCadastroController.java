12d2472e82a683b12df70f60d4d10bd6e972126c
matheus-vazzz <mvaz41322@gmail.com>
2024-11-26 19:31:40 -0300

    Add files via upload

---
+++ controller/TelaDeCadastroController.java
@@ -0,0 +1,9 @@
+package controller;
+import view.*;
+import model.*;
+
+public class TelaDeCadastroController extends TelaDeCadastroView {
+    public static void cadastrarController() {
+        TelaDeCadastroModel.cadastrarModel(txtNome.getText(), txtEmail.getText(), String.valueOf(txtSenha.getPassword()));
+    }
+}
